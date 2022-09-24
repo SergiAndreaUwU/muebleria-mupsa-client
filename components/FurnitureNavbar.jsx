d0f10d40470180ade320449e48b6e38346db5ba0
Sergio Salazar <sso@bbpingod.com>
2022-09-24 14:43:36 -0500

    added some colors to navbar

--- components/FurnitureNavbar.jsx
+++ components/FurnitureNavbar.jsx
@@ -2,10 +2,11 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import styles from "./FurnitureNavbar.module.sass"
 
 function FurnitureNavbar() {
   return (
-    <Navbar bg="light" expand="lg">
+    <Navbar bg="light" expand="lg" className={styles.navbar}>
       <Container>
         <Navbar.Brand href="/">MUPSA</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
