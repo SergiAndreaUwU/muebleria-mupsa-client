f761582608c17ea7004d14c226ed4a659ae81389
Sergio Salazar <sso@bbpingod.com>
2022-09-26 23:37:27 -0500

    added responsiveness to gallery

--- components/CatalogueCarousel.jsx
+++ components/CatalogueCarousel.jsx
@@ -1,28 +1,28 @@
 import React from "react";
 import Carousel from "react-bootstrap/Carousel";
 import Image from "next/image";
+import { Container } from "react-bootstrap";
 
 function CatalogueCarousel({ imagesArray }) {
   return (
-    <div>
+    <Container>
       <Carousel variant="dark" indicators={false}>
         {imagesArray.map((imag, index) => (
           <Carousel.Item key={imag}>
             <div style={{ display: "flex" }}>
-              <div style={{ margin: "auto" }}>
+              <div style={{ margin: "auto",width: '100%', height: '85vh', position: 'relative' }}>
                 <Image
                   src={imag}
                   alt={`${index} slide`}
+                  layout="fill"
                   objectFit="contain"
-                  width={800}
-                  height={800}
                 />
               </div>
             </div>
           </Carousel.Item>
         ))}
       </Carousel>
-    </div>
+    </Container>
   );
 }
 
